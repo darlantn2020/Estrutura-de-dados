0b5694730de2f48a8598e2d13d733fed79e27f65
darlantn2020 <61881539+darlantn2020@users.noreply.github.com>
2021-02-23 21:12:17 -0300

    Add files via upload

---
+++ Lista Diagnostica Revisão/Lista Diagnostica Revisão/Ex3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int funcao_recurssiva(int x,int y){
+  if(y == 0){
+    return 1;
+  }else{
+    return x * funcao_recurssiva(x,y-1);
+  }//fim else
+}//fim funcao_recurssiva
+int main (){
+
+  int x;
+  int y;
+  int recurssiva;
+
+  printf("Base:\n");
+  scanf("%i",&x);
+  printf("Expoente\n");
+  scanf("%i",&y);
+  recurssiva = funcao_recurssiva(x,y);
+  printf("Resultado da base %i elevado ao expoente %i eh igual a: %i\n",x,y,recurssiva );
+
+}//fim main
