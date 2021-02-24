32c2fbcee4e2264fd3a1e8ce7620c3adac5d885f
darlantn2020 <61881539+darlantn2020@users.noreply.github.com>
2021-02-23 21:15:20 -0300

    Add files via upload

--- Lista Diagnostica Revisão/Ex1.c
+++ Lista Diagnostica Revisão/Ex1.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int funcao_recurssiva(int x,int y){
-  if(y == 0){
-    return 1;
-  }else{
-    return x * funcao_recurssiva(x,y-1);
-  }
-}
-
+void funcao_total_minutos(int* hora1,int* minutos1,int* Total_minutos1)
+  {
+    *Total_minutos1 = *hora1*60 + *minutos1;
+  }//fim funcao_total_minutos
 int main (){
 
-  int x;
-  int y;
-  int recurssiva;
+    int Total_minutos;
+    int horas;
+    int minutos;
 
-  printf("Base:\n");
-  scanf("%i",&x);
-  printf("Expoente\n");
-  scanf("%i",&y);
-  recurssiva = funcao_recurssiva(x,y);
-  printf("Resultado da base %i elevado ao expoente %i eh igual a: %i\n",x,y,recurssiva );
-
+    printf("Quantas horas?\n");
+    scanf("%i",&horas);
+    do {
+        printf("Quantas minutos?\n");
+        scanf("%i",&minutos);
+        if(minutos>60){
+        printf("Valor muito alto eh de 0 a 60\n\n");
+      }//fim if
+    }while(minutos>=60);//fim do while
+    funcao_total_minutos(&horas,&minutos,&Total_minutos);
+    printf("Sao %i minutos\n",Total_minutos);
 }//fim main
